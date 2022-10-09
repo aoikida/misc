32f5ccaf7122d4b0699cd94881efc1970285b512
aoikida <t20249ak@sfc.keio.ac.jp>
2022-10-09 22:41:25 +0900

    Change some

--- merge_sort/merge_sort.cc
+++ merge_sort/merge_sort.cc
@@ -6,6 +6,7 @@ void MergeSort(vector<int> &A, int left, int right) {
 
     //これ以上分割できない地点
     if (right - left == 1)return;
+
     int middle = left + (right - left) / 2;
 
     //左半分をsort
@@ -13,13 +14,14 @@ void MergeSort(vector<int> &A, int left, int right) {
     //右半分をsort
     MergeSort(A, middle, right);
 
+    //外部メモリにコピー
     vector<int> buf;
     for(int i = left; i < middle; ++i) buf.push_back(A[i]);
     for(int i = right - 1; i >= middle; --i) buf.push_back(A[i]);
 
     //左と右を併合する
-    int index_left = 0; //左側の添字
-    int index_right = (int)buf.size() - 1; //右側の添字
+    int index_left = 0; 
+    int index_right = (int)buf.size() - 1; 
 
     for(int i = left; i < right; ++i) {
         //左側採用
@@ -34,10 +36,10 @@ void MergeSort(vector<int> &A, int left, int right) {
 }
 
 int main() {
-
-    //入力
     int N = 0;
     vector<int> A(N);
+
+    //入力
     cin >> N;
     for(int i = 0; i < N; ++i) cin >> A[i];
 
