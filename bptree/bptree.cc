1bd17da57cac02ededcf2f84d9b4fe54b4c1ef91
aoikida <t20249ak@sfc.keio.ac.jp>
2022-10-20 17:50:49 +0900

    Add bptree

---
+++ bptree/bptree.cc
@@ -0,0 +1,181 @@
+#include "bptree.h"
+#include <vector>
+#include <sys/time.h>
+
+struct timeval
+cur_time(void)
+{
+	struct timeval t;
+	gettimeofday(&t, NULL);
+	return t;
+}
+
+void
+print_tree_core(NODE *n)
+{
+	printf("["); 
+	for (int i = 0; i < n->nkey; i++) {
+		if (!n->isLeaf) print_tree_core(n->chi[i]); 
+		printf("%d", n->key[i]); 
+		if (i != n->nkey-1 && n->isLeaf) putchar(' ');
+	}
+	if (!n->isLeaf) print_tree_core(n->chi[n->nkey]);
+	printf("]");
+}
+
+void
+print_tree(NODE *node)
+{
+	print_tree_core(node);
+	printf("\n"); fflush(stdout);
+}
+
+NODE *
+find_leaf(NODE *node, int key)
+{
+	int kid;
+
+	if (node->isLeaf) return node;
+	for (kid = 0; kid < node->nkey; kid++) {
+		if (key < node->key[kid]) break;
+	}
+
+	return find_leaf(node->chi[kid], key);
+}
+
+NODE *
+insert_in_leaf(NODE *leaf, int key, DATA *data)
+{
+	int i;
+	// 
+	if (key < leaf->key[0]) {
+		for (i = leaf->nkey; i > 0; i--) {
+			leaf->chi[i] = leaf->chi[i-1] ;
+			leaf->key[i] = leaf->key[i-1] ;
+		} 
+		leaf->key[0] = key;
+		leaf->chi[0] = (NODE *)data;
+	}
+	else {
+		for (i = 0; i < leaf->nkey; i++) {
+			if (key < leaf->key[i]) break;
+		}
+		for (int j = leaf->nkey; j > i; j--) {		
+			leaf->chi[j] = leaf->chi[j-1] ;
+			leaf->key[j] = leaf->key[j-1] ;
+		} 
+
+    	/* CodeQuiz */
+		leaf->key[i] = key;
+		leaf->chi[i] = (NODE *)data;
+
+	}
+	leaf->nkey++;
+
+	return leaf;
+}
+
+NODE * 
+insert_in_parent(NODE *leaf, int key, NODE *new_leaf)
+{
+	
+}
+
+NODE *
+alloc_leaf(NODE *parent)
+{
+	NODE *node;
+	if (!(node = (NODE *)calloc(1, sizeof(NODE)))) ERR;
+	node->isLeaf = true;
+	node->parent = parent;
+	node->nkey = 0;
+
+	return node;
+}
+
+void 
+insert(int key, DATA *data)
+{
+	NODE *leaf;
+
+	if (Root == NULL) {
+		leaf = alloc_leaf(NULL);
+		Root = leaf;
+	}
+  	else {
+    	leaf = find_leaf(Root, key);
+  	}
+
+	if (leaf->nkey < (N-1)) {
+		insert_in_leaf(leaf, key, data);
+	}
+	else {
+		NODE *new_leaf = alloc_leaf(NULL);
+		TEMP *temp_block = (TEMP*)alloc_leaf(NULL);
+
+		for(int i = 0; i < leaf->nkey; i++){
+			temp_block->chi[i] = leaf->chi[i];
+			temp_block->key[i] = leaf->key[i];
+		}
+
+		insert_in_leaf((NODE*)temp_block, key, data);
+
+		new_leaf->chi[leaf->nkey] = leaf->chi[leaf->nkey];
+		leaf->chi[leaf->nkey] = new_leaf;
+
+		for(int i = 0; i < leaf-> nkey; i++){
+			leaf->chi[i] = 0;
+			leaf->key[i] = 0;
+		}
+
+		for(int i = 0; i<= (leaf->nkey / 2); i++){
+			leaf->chi[i] = temp_block->chi[i];
+			leaf->key[i] = temp_block->key[i];
+		}
+
+		for(int i = (leaf->nkey / 2); i< leaf->nkey; i++){
+			new_leaf->chi[i] = temp_block->chi[i];
+			new_leaf->key[i] = temp_block->key[i];
+		}
+
+		int smallest_key = new_leaf->key[leaf->nkey / 2];
+
+		insert_in_parent(leaf, smallest_key, new_leaf);
+
+	}
+}
+
+void
+init_root(void)
+{
+	Root = NULL;
+}
+
+int 
+interactive()
+{
+  int key;
+
+  std::cout << "Key: ";
+  std::cin >> key;
+
+  return key;
+}
+
+int
+main(int argc, char *argv[])
+{
+  struct timeval begin, end;
+
+	init_root();
+
+	printf("-----Insert-----\n");
+	begin = cur_time();
+  	while (true) {
+		insert(interactive(), NULL);
+    	print_tree(Root);
+  	}
+	end = cur_time();
+
+	return 0;
+}
