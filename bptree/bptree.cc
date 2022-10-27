5e6a1d08254d14e303a7a698e5e52a6dd4436a21
aoikida <t20249ak@sfc.keio.ac.jp>
2022-10-27 21:28:52 +0900

    Bug fix

--- bptree/bptree.cc
+++ bptree/bptree.cc
@@ -2,6 +2,10 @@
 #include <vector>
 #include <sys/time.h>
 
+NODE *alloc_leaf(NODE *parent);
+NODE *insert_in_leaf(NODE *leaf, int key, DATA *data);
+
+
 struct timeval
 cur_time(void)
 {
@@ -15,7 +19,7 @@ print_tree_core(NODE *n)
 {
 	printf("["); 
 	for (int i = 0; i < n->nkey; i++) {
-		if (!n->isLeaf) print_tree_core(n->chi[i]); 
+		if (!n->isLeaf) print_tree_core(n->chi[i]);
 		printf("%d", n->key[i]); 
 		if (i != n->nkey-1 && n->isLeaf) putchar(' ');
 	}
@@ -47,7 +51,7 @@ NODE *
 insert_in_leaf(NODE *leaf, int key, DATA *data)
 {
 	int i;
-	// 
+	
 	if (key < leaf->key[0]) {
 		for (i = leaf->nkey; i > 0; i--) {
 			leaf->chi[i] = leaf->chi[i-1] ;
@@ -65,7 +69,6 @@ insert_in_leaf(NODE *leaf, int key, DATA *data)
 			leaf->key[j] = leaf->key[j-1] ;
 		} 
 
-    	/* CodeQuiz */
 		leaf->key[i] = key;
 		leaf->chi[i] = (NODE *)data;
 
@@ -75,10 +78,154 @@ insert_in_leaf(NODE *leaf, int key, DATA *data)
 	return leaf;
 }
 
-NODE * 
-insert_in_parent(NODE *leaf, int key, NODE *new_leaf)
+TEMP *
+insert_in_temp_leaf(TEMP *leaf, int key, DATA *data)
 {
-	
+	int i;
+	if (key < leaf->key[0]) {
+		for (i = leaf->nkey; i > 0; i--) {
+			leaf->chi[i] = leaf->chi[i-1] ;
+			leaf->key[i] = leaf->key[i-1] ;
+		}
+		leaf->key[0] = key;
+		leaf->chi[0] = (NODE *)data;
+	}
+	else {
+		for (i = 0; i < leaf->nkey; i++) {
+			if (key < leaf->key[i]) break;
+		}
+		for (int j = leaf->nkey; j > i; j--) {
+			leaf->chi[j] = leaf->chi[j-1] ;
+			leaf->key[j] = leaf->key[j-1] ;
+		}
+
+		leaf->key[i] = key;
+		leaf->chi[i] = (NODE *)data;
+
+	}
+
+	leaf->nkey++;
+
+	return leaf;
+}
+
+void
+insert_in_internal(NODE *internal, int key, DATA *data)
+{
+	int i;
+
+	if (key < internal->key[0]) {
+		for (i = internal->nkey; i > 0; i--) {
+			internal->key[i] = internal->key[i-1];
+			internal->chi[i+1] = internal->chi[i];
+		}
+		internal->key[0] = key;
+		internal->chi[1] = (NODE*)data;
+
+	}
+	else {
+		for (i = 0; i < internal->nkey; i++) {
+			if (key < internal->key[i]) break;
+		}
+		for (int j = internal->nkey; j > i; j--) {
+			internal->key[j] = internal->key[j-1];
+			internal->chi[j+1] = internal->chi[j];
+		}
+
+		internal->key[i] = key;
+		internal->chi[i+1] = (NODE*)data;
+
+	}
+
+	internal->nkey++;
+
+	return;
+}
+
+NODE *
+insert_in_parent(NODE *leaf, int parent_key, NODE *new_leaf)
+{	
+	int i;
+	NODE *parent_node = NULL;
+
+	if (leaf == Root) {
+		parent_node = alloc_leaf(NULL);
+		parent_node->isLeaf = false;
+		parent_node->chi[0] = leaf;
+		parent_node->chi[1] = new_leaf;
+		parent_node->key[0] = parent_key;
+		parent_node->nkey = 1;
+		Root = parent_node;
+		leaf->parent = Root;
+		new_leaf->parent = Root;
+
+		return parent_node;
+	}
+
+	parent_node = Root;
+	new_leaf->parent = Root;
+
+	if (parent_node->nkey < N - 1){
+		//debag
+		for(int i = 0; i < N; i++){
+			printf("internal->key = %d\n", parent_node->key[i]);
+		}
+		printf("--------------------------\n");
+		insert_in_internal(parent_node, parent_key, (DATA *)new_leaf);
+		//debag
+		for(int i = 0; i < N; i++){
+			printf("internal->key = %d\n", parent_node->key[i]);
+		}
+	}
+
+	else { //ここがおかしい
+
+		TEMP *temp_block = (TEMP*)alloc_leaf(NULL);
+		temp_block->isLeaf = true;
+
+		for(i = 0; i < N - 1; i++){
+			temp_block->key[i] = parent_node->key[i];
+			parent_node->key[i] = 0;
+		}
+		for(i = 0; i < N; i++){
+			temp_block->chi[i] = parent_node->chi[i];
+			parent_node->chi[i] = 0;
+		}
+
+		insert_in_leaf((NODE *)temp_block, parent_key, (DATA *)new_leaf);
+		
+		NODE *new_parent_node = alloc_leaf(NULL);
+		new_parent_node->isLeaf = false;
+
+		for(i = 0; i < (N/2); i++){
+			parent_node->key[i] = temp_block->key[i];
+		}
+		for(i = 0; i < (N/2)+1; i++){
+			parent_node->chi[i] = temp_block->chi[i];
+		}
+
+		parent_node->nkey = 2;
+
+		for(i = (N/2)+1; i < N; i++){
+			new_parent_node->key[i - (N/2) - 1] = temp_block->key[i];
+		}
+
+		for(i=(N/2)+1; i<N+1; i++){
+			new_parent_node->chi[i-(N/2)-1] = temp_block->chi[i];
+			new_parent_node->chi[i-(N/2)-1]->parent = new_parent_node;
+		}
+
+		new_parent_node->nkey = 1;
+
+		int grand_parent_key = temp_block->key[(N+1)/2];
+
+		free(temp_block);
+		temp_block = NULL;
+
+		insert_in_parent(parent_node, grand_parent_key, new_parent_node);
+
+	}
+	return parent_node;	
 }
 
 NODE *
@@ -106,41 +253,54 @@ insert(int key, DATA *data)
     	leaf = find_leaf(Root, key);
   	}
 
-	if (leaf->nkey < (N-1)) {
+	if (leaf->nkey < N-1) {
 		insert_in_leaf(leaf, key, data);
 	}
 	else {
 		NODE *new_leaf = alloc_leaf(NULL);
 		TEMP *temp_block = (TEMP*)alloc_leaf(NULL);
 
-		for(int i = 0; i < leaf->nkey; i++){
+		for(int i = 0; i < N - 1; i++){
 			temp_block->chi[i] = leaf->chi[i];
 			temp_block->key[i] = leaf->key[i];
 		}
 
-		insert_in_leaf((NODE*)temp_block, key, data);
+		temp_block->nkey = 3;
 
-		new_leaf->chi[leaf->nkey] = leaf->chi[leaf->nkey];
-		leaf->chi[leaf->nkey] = new_leaf;
+		insert_in_temp_leaf(temp_block, key, data);
 
-		for(int i = 0; i < leaf-> nkey; i++){
+		//debag
+		for(int i = 0; i < N; i++){
+			printf("temp_block->key = %d\n", temp_block->key[i]);
+		}
+
+		new_leaf->chi[N - 1] = leaf->chi[N - 1];
+		leaf->chi[N - 1] = new_leaf;
+
+		for(int i = 0; i < N - 1; i++){
 			leaf->chi[i] = 0;
 			leaf->key[i] = 0;
 		}
+		leaf->nkey = 2;
 
-		for(int i = 0; i<= (leaf->nkey / 2); i++){
+		for(int i = 0; i < (N/2); i++){
 			leaf->chi[i] = temp_block->chi[i];
 			leaf->key[i] = temp_block->key[i];
 		}
 
-		for(int i = (leaf->nkey / 2); i< leaf->nkey; i++){
-			new_leaf->chi[i] = temp_block->chi[i];
-			new_leaf->key[i] = temp_block->key[i];
+		for(int i = (N/2); i < N; i++){
+			new_leaf->chi[i-(N/2)] = temp_block->chi[i];
+			new_leaf->key[i-(N/2)] = temp_block->key[i];
 		}
+		new_leaf->nkey = 2;
+		new_leaf->isLeaf = true;
+		
+		free(temp_block);
+		temp_block = NULL;
 
-		int smallest_key = new_leaf->key[leaf->nkey / 2];
+		int parent_key = new_leaf->key[0];
 
-		insert_in_parent(leaf, smallest_key, new_leaf);
+		insert_in_parent(leaf, parent_key, new_leaf);
 
 	}
 }
